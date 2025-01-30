a5ae263ec81b1c6f4afdea037b65ca6bd277b505
marcuswentz <markzcollege404@gmail.com>
2025-01-29 22:45:15 -0500

    Center text

--- app/page.tsx
+++ app/page.tsx
@@ -250,7 +250,7 @@ export default function Home() {
  
    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-decimal text-sm text-center sm:text-center font-[family-name:var(--font-geist-mono)]">
        
          <div>
           <h4> </h4>
